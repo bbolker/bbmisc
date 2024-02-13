dda6f38c78faf95b5c455f25740ad4472f4acb2b
Ben Bolker <bbolker@gmail.com>
2024-02-12 19:54:25 -0500

    fix SESOI sims

--- Rmisc/sim_sesoi.R
+++ Rmisc/sim_sesoi.R
@@ -1,8 +1,6 @@
 ## mostly trying to avoid tidyverse but ...
-library(ggplot2); theme_set(theme_bw())
+library(ggplot2); theme_set(theme_bw() + theme(panel.spacing=grid::unit(0, "lines")))
                  
-
-
 ## simulate a t-test-like comparison: what is the distribution of outcomes/
 ## probability of Dushoff outcomes
 ## https://dushoff.github.io/ResearchSandbox/clarStrength.Rout.pdf
@@ -20,35 +18,31 @@ tt$power ## 0.807
 ## two-group sim for equal-var t-test power/outcome calculation
 simfun <- function(n, delta=1, sd=1, conf.level = 0.95, seed = NULL) {
     if (!is.null(seed)) set.seed(seed)
-    x <- replicate(2, rnorm(n, mean = delta, sd = sd), simplify = FALSE)
-    tt <- t.test(x[[1]], x[[2]], conf.level = conf.level)
+    x <- rnorm(2*n, mean = rep(c(0,delta), each =n), sd = sd)
+    tt <- t.test(x[1:n], x[-(1:n)], conf.level = conf.level, var.equal = TRUE)
     with(tt, c(est = unname(-1*diff(estimate)),
                         lwr = conf.int[1], upr = conf.int[2]))
 }    
 
-levs <- c("large/positive",
-          "large?/positive",
-          "small/positive",
-          "small/positive?",
-          "not (large&negative)",
+levs <- c("large/clear sign",
+          "unclear magnitude/clear sign",
+          "small/clear sign",
+          "small/unclear sign",
+          "NOT (large and opposite est)",
           "unclear")
 
-catfun <- function(x, delta=1) {
-    m <- mean(x[2:3])
-    ci <- x[2:3]
-    ## adjust for symmetry
-    ## FIXME: eventually, do this some other way
-    ci <- ci*sign(m)
-    m <- abs(m)
-    lwr <- ci[1]
-    upr <- ci[2]
+catfun <- function(x, s=1) {
+    lwr <- x[2]
+    upr <- x[3]
+    ## adjust for symmetry? ci <- ci*sign(m); m <- abs(m)
     ## case-when?
-    if (lwr>delta) return(levs[1])
-    if (m > delta & lwr>0) return(levs[2])
-    if (lwr>0 & upr < delta) return(levs[3])
-    if (lwr<0 & upr < delta) return(levs[4])
-    if (lwr>(-delta) & upr>delta) return(levs[5])
-    if (lwr<(-delta) & upr>delta) return(levs[6])
+    if (lwr>s || upr<(-s)) return(levs[1])
+    if ((upr>s && lwr>0 && lwr<s) || (lwr<(-s) && upr<0 && upr>(-s))) return(levs[2])
+    if ((lwr>0 && upr<s) || (upr<0 && lwr>(-s))) return(levs[3])
+    if ((lwr>(-s) && lwr<0 && upr>0 && upr<s) ||
+        (upr<s   && upr>0 && lwr>0 && upr>(-s))) return(levs[4])
+    if ((lwr<0 && lwr>(-s) && upr>s) || (upr>0 && upr<s && lwr<(-s))) return(levs[5])
+    if (lwr<(-s) && upr>s) return(levs[6])
 }
 
 
@@ -60,19 +54,29 @@ system.time({
 })
 
 ## flip sign
-dd1 <- transform(dd1, lwr = sign(est)*lwr, upr = sign(est)*upr,
-                 est = abs(est))
+## dd1 <- transform(dd1, lwr = sign(est)*lwr, upr = sign(est)*upr,
+## est = abs(est))
 ## ordering for caterpillar plot
+## alternately could arrange() and then fct_inorder
 dd1$n <- reorder(factor(seq(nrow(dd1))), dd1$est)
 
-print(ggplot(dd1, aes(n, est))
+
+print(gg0 <- ggplot(dd1, aes(n, est))
       + geom_pointrange(aes(colour = cat, ymin = lwr, ymax = upr),
                         alpha = 0.5)
+      + geom_hline(yintercept = 0)
+      + geom_hline(yintercept = c(-1, 1), linetype = 2)
       + scale_x_discrete(labels = NULL, breaks = NULL))
 
-props <- table(dd1$cat) |> prop.table()
-print(power <- sum(props[1:3]))  ## why only 0.5, not 0.8?
-## (something about two-sidedness?)
+print(gg0 + facet_wrap(~cat))
+## plot looks plausible
+
+print(props <- table(dd1$cat) |> prop.table())
+print(power <- sum(props[1:3]))
+
+## what if the true effect size is twice the SESOI/powered effect size?
+dd1$cat2 <- apply(dd1, 1, catfun, s=0.5) |> factor(levels = levs)
+print(props2 <- table(dd1$cat2) |> prop.table())
 
 ## note: this is the body of the t-test power calculation
 ## where tsample is 1 for one-sample, 2 for two-sample
@@ -87,6 +91,9 @@ print(power <- sum(props[1:3]))  ## why only 0.5, not 0.8?
 ##     pt(-qu, nu, ncp = sqrt(n/tsample) * delta/sd, 
 ##        lower.tail = TRUE)
 
+## non-central t based on two different centers??
+
 ## our problem has *two* scales (we should be able to manipulate
 ##  the effect size and the cutoff separately!
 ## making effect size = SESOI = cutoff is (over?)simplifying the problem
+
