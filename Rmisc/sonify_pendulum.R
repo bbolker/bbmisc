b28466439b6cc6c3373a5650b8727d21bc242cd3
Ben Bolker <bbolker@gmail.com>
2024-09-06 19:00:43 -0400

    pendulum

--- Rmisc/sonify_pendulum.R
+++ Rmisc/sonify_pendulum.R
@@ -30,22 +30,23 @@ pars <- c(g=9.8, l1=1, l2=1, m1 = 1, m2 = 1)
 y0 <- c(th1=pi/4, th2=pi/4, om1=0, om2=0)
 ## need more extreme starting values for chaos)
 y1 <- c(th1=3*pi/4, th2=-3*pi/4, om1=0, om2=0)
-grad(0, y1, pars)
+## grad(0, y1, pars)
 
 pert <- c(0.001, 0, 0, 0)
 res1 <- as.data.frame(ode(y1, seq(0, 20, by = 0.1), grad, pars))
 res2 <- as.data.frame(ode(y1+pert, seq(0, 20, by = 0.1), grad, pars))
 
-f <- function(d) with(d, cos(th1)+cos(th2)/2)
+calc_y <- function(d) with(d, cos(th1)+cos(th2)/2)
 
 matplot(cbind(res1[,-1], res2[,-1]), type = "l",
         col = rep(1:4, 2), lty= rep(1:2, each = 4))
 
-matplot(cbind(f(res1), f(res2)), type = "l", lty = 1:2)
+matplot(cbind(calc_y(res1), calc_y(res2)), type = "l", lty = 1:2)
+tvec <- res1$time
 
 ## stereo = FALSE?
-s1 <- with(res1, sonify(time, th1, play = FALSE))
-s2 <- with(res2, sonify(time, th1, waveform = "sawtooth", play = FALSE))
+s1 <- sonify(tvec, calc_y(res1), waveform = "square", play = FALSE)
+s2 <- sonify(tvec, calc_y(res2), waveform = "sawtooth", play = FALSE)
 
 ## how do we combine these?? multichannel/etc. ?
 tuneR::writeWave(s1, "tmp1.wav")
@@ -53,16 +54,33 @@ tuneR::writeWave(s2, "tmp2.wav")
 system("xdg-open tmp1.wav")
 system("xdg-open tmp2.wav")
 
-## adding seems to work ...
-s12 <- s1
-s12@.Data <- s1@.Data + s2@.Data
-s12 <- normalize(s12, unit = "16")
-tuneR::writeWave(s12, "tmp12.wav")
-system("xdg-open tmp12.wav")
+s_comb <- function(s1, s2) {
+    ## adding seems to work ...
+    s12 <- s1
+    s12@.Data <- s1@.Data + s2@.Data
+    return(normalize(s12, unit = "16"))
+}
 
-## FIXME
-## * pitches seem different -- different harmonics emphasized?
-## * explain/understand meanings of variables (see W'pedia page)
-## * normalize angles somehow/switch back to x, y coordinates?
+s_lrcomb <- function(s1, s2) {
+    ## adding seems to work ...
+    s12 <- s1
+    s12@.Data[] <- cbind(s1@.Data[,"FR"], s2@.Data[,"FL"])
+    return(normalize(s12, unit = "16"))
+}
+
+tuneR::writeWave(s_comb(s1, s2), "timbres.wav")
+system("xdg-open timbres.wav")
+
+s1L <- sonify(tvec, calc_y(res1), waveform = "square", play = FALSE,
+             stereo = FALSE)
+s2R <- sonify(tvec, calc_y(res2), waveform = "sawtooth", play = FALSE,
+              stereo = FALSE)
+tuneR::writeWave(s_comb(s1L, s2R), "timbres_LR.wav")
+system("xdg-open timbres_LR.wav")
 ## * distinguish pendula via octave/ left vs right channel / volume /timbre?
+
+s1L_low <- sonify(tvec, calc_y(res1), waveform = "square", play = FALSE,
+             stereo = FALSE, flim = c(220, 440))
+tuneR::writeWave(s_comb(s1L_low, s2R), "timbres_LR_oct.wav")
+system("xdg-open timbres_LR_oct.wav")
 
